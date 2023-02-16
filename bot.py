a92bfb2d33d3448ab4a2a24161e71175598e9855
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-02-16 13:47:22 +0200

    added SQL adapter

---
+++ bot.py
@@ -0,0 +1,21 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
+chatbot = ChatBot("Friday", storage_adapter="chatterbot.storage.SQLStorageAdapter", database_uri="", read_only=True)
+
+#trainer = ListTrainer(chatbot)
+#trainer.train(sample_conversation)
+
+chatbot.set_trainer(ChatterBotCorpusTrainer)
+chatbot.train("chatterbot.corpus.english", "chatterbot.corpus.english.greetings", "chatterbot.corpus.english.conversations")
+
+exit_tuple = ('exit', 'see you later', 'bye')
+while True:
+    request = input('You: ')
+    if request.lower() in exit_tuple:
+        print('Friday: Bye')
+        break
+    else:
+        response = chatbot.get_response(request)
+        print('Friday: ', response)
