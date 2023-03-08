f91353dc15fa0de964915cf21d962d37bd37acd4
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-03-08 06:52:53 +0200

    integrating client chats with corpus data

--- bot.py
+++ bot.py
@@ -8,7 +8,15 @@ friday = ChatBot("Friday")
 #trainer.train(sample_conversation)
 
 friday.set_trainer(ChatterBotCorpusTrainer)
-friday.train("chatterbot.corpus.english", "chatterbot.corpus.english.greetings", "chatterbot.corpus.english.conversations")
+friday.train("chatterbot.corpus.english",
+             "chatterbot.corpus.english.botprofile",
+             "chatterbot.corpus.english.computers",
+             "chatterbot.corpus.english.emotion",
+             "chatterbot.corpus.english.food",
+             "chatterbot.corpus.english.gossip",
+             "chatterbot.corpus.english.science",
+             "chatterbot.corpus.english.greetings",
+             "chatterbot.corpus.english.conversations")
 
 exit_tuple = ('exit', 'see you later', 'bye')
 while True:
