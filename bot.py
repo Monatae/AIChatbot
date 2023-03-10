121b0f0bb321b5543dcd87b1171d3ffaec4ff560
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-03-10 17:25:24 +0200

    better cleaning of input

--- bot.py
+++ bot.py
@@ -4,26 +4,19 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 
 friday = ChatBot("Friday")
 
-#trainer = ListTrainer(chatbot)
-#trainer.train(sample_conversation)
+
+
 
 friday.set_trainer(ChatterBotCorpusTrainer)
 friday.train("chatterbot.corpus.english",
-             "chatterbot.corpus.english.botprofile",
-             "chatterbot.corpus.english.computers",
-             "chatterbot.corpus.english.emotion",
-             "chatterbot.corpus.english.food",
-             "chatterbot.corpus.english.gossip",
-             "chatterbot.corpus.english.science",
-             "chatterbot.corpus.english.greetings",
-             "chatterbot.corpus.english.conversations")
+            "chatterbot.corpus.english.quries")
 
 exit_tuple = ('exit', 'see you later', 'bye')
 while True:
-    request = input('You: ')
-    if request.lower() in exit_tuple:
+    user_input = input('You: ')
+    if user_input.lower() in exit_tuple:
         print('Friday: Bye')
         break
     else:
-        response = friday.get_response(request)
+        response = friday.get_response(user_input.replace('You: ', '', 1))
         print('Friday: ', response)
