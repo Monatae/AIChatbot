43d4aec5615d1d13d76e021eab1e460453535b84
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-04-06 20:55:58 +0200

    flask webhook working properly

--- bot.py
+++ bot.py
@@ -14,24 +14,15 @@ client = Client(account_sid, auth_token)
 
 #setting up connection to the speech recognition server
 server_ip = '127.0.0.1'
-server_port = 5000
+server_port = 5001
 server_address = (server_ip, server_port)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#sock.connect((server_address))
+sock.connect((server_address))
 ####this is where sendaudio and receive_transcript functions come into play#####
 
 
 #setting up the chatbot
-friday = ChatBot("Friday",
-                 read_only = True,
-                 storage_adapter ="chatterbot.storage.SQLStorageAdapter",
-                 database_uri="sqlite:///database.sqlite3"
-                 )
 
-#training the chatbot
-friday.set_trainer(ChatterBotCorpusTrainer)
-friday.train("chatterbot.corpus.english.quries", "chatterbot.corpus.english.greetings", "chatterbot.corpus.english.conversations")
-exit_tuple = ('exit', 'see you later', 'bye')
 
 
 
@@ -39,6 +30,16 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    friday = ChatBot("Friday",
+                 read_only = True,
+                 storage_adapter ="chatterbot.storage.SQLStorageAdapter",
+                 database_uri="sqlite:///database.sqlite3"
+                 )
+
+    #training the chatbot
+    friday.set_trainer(ChatterBotCorpusTrainer)
+    friday.train("chatterbot.corpus.english.quries", "chatterbot.corpus.english.greetings", "chatterbot.corpus.english.conversations")
+    exit_tuple = ('exit', 'see you later', 'bye')
     incoming_msg = request.values.get('Body', '')
     resp = MessagingResponse()
     # Use Chatterbot to generate a response
@@ -51,13 +52,16 @@ def webhook():
                 break
             else:
                 response = friday.get_response(incoming_msg)
-                resp.message(response)
+                statement = response.text
+                resp.message(statement)
+                break
+            
     return str(resp)
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
 
 
         
--- soundrecon.py
+++ soundrecon.py
@@ -5,7 +5,7 @@ from google.cloud import speech
 
 # setting up socket engine
 server_ip = "127.0.0.1"
-server_port = 9999
+server_port = 5001
 server_address = (server_ip, server_port)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(server_address)
@@ -19,13 +19,10 @@ filesize = connection.recv(1024).decode()
 print("Connection from", client_address)
 print(filename)
 print(filesize)
-
-
 received_file = open(filename, "wb")
 filebytes = b""
 
 done = False
-
 progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(filesize))
 
 while not done:
@@ -44,18 +41,15 @@ sleep(10)
 
 # speech recognition
 client = speech.SpeechClient.from_service_account_file("key.json")
-
 with open("test.wav", "rb") as f:
     content = f.read()
-
-audio_file = speech.RecognitionAudio(content=content)
-config = speech.RecognitionConfig(
-    sample_rate_hertz=16000, enable_automatic_punctuation=True, language_code="en-US"
-)
-
-response = client.recognize(config=config, audio=audio_file)
-
-
-for result in response.results:
-    print("Transcript: {}".format(result.alternatives[0].transcript))
-    connection.send("{}".format(result.alternatives[0].transcript).encode())
+    audio_file = speech.RecognitionAudio(content=content)
+    config = speech.RecognitionConfig(
+        sample_rate_hertz=16000,
+        enable_automatic_punctuation=True,
+        language_code="en-US"
+    )
+    response = client.recognize(config=config, audio=audio_file)
+    for result in response.results:
+        print("Transcript: {}".format(result.alternatives[0].transcript))
+        connection.send("{}".format(result.alternatives[0].transcript).encode())
