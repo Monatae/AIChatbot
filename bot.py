add05163c928d7b896f519bea66d0c0463f15e2f
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-04-08 18:51:24 +0200

    added google dialogflow as new engine

--- bot.py
+++ bot.py
@@ -1,17 +1,17 @@
 from twilio.rest import Client
-import dialogflow_v2 as dialogflow
+from google.cloud import dialogflow_v2 as dialogflow
 from google.api_core.exceptions import InvalidArgument
 import os
 
 # Set up the Twilio client
-account_sid = "YOUR_ACCOUNT_SID"
-auth_token = "YOUR_AUTH_TOKEN"
+account_sid = "AC81c0755065e9eba6542ca94a9571ca8e"
+auth_token = "63a4d1fa607bc9ef2fa7b1525bb3a6a7"
 client = Client(account_sid, auth_token)
 
 # Set up the Dialogflow client
-DIALOGFLOW_PROJECT_ID = "YOUR_PROJECT_ID"
+DIALOGFLOW_PROJECT_ID = "ordinal-gear-381907"
 DIALOGFLOW_LANGUAGE_CODE = "en"
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/path/to/your/credentials.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "ordinal-gear-381907-452922c5eb30.json"
 session_client = dialogflow.SessionsClient()
 
 
@@ -23,8 +23,10 @@ def process_message(from_number, body):
 
     # Send the user's message to Dialogflow
     text_input = dialogflow.TextInput(text=body, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    # Making a query using against dialogflow out of the text
     query_input = dialogflow.QueryInput(text=text_input)
     try:
+        # detecting intent of the query
         response = session_client.detect_intent(
             session=session, query_input=query_input
         )
@@ -55,7 +57,23 @@ app = Flask(__name__)
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    # Parse the incoming Twilio message
+    """
+    so the logic of our app is:
+
+    when end-user sends a whatsapp message to our twilio sandbox, twilio makes an HTTP POST call to our application
+
+
+
+        1. You call this webhook via the webhook's endpoint placed in Twilio (URL --> in this case it is a HTTP POST request on the flaskapp's address, port number and its flaskapp route name)
+        2. The webhook then strips some details from the HTTP POST request's body i.e from number and body of the request
+        3. The webhook then takes the from-phonenumber and the body of the message for processing
+        4. The from-phonenumber is used to create a unique session ID while the body is used to generate queries against google diaglog flow
+        5. The webhook then takes the response which is returned by the querying the google dialog flow from point [4], and sends it as a message back to the from-phone number
+        6. After executing the above 5 steps, the webhook returns OK as an HTTP status code
+
+    the end
+    """
+    # Parse the incoming Twilio message from the http post request using request lib
     from_number = request.form["From"]
     body = request.form["Body"]
 
@@ -70,4 +88,4 @@ def webhook():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
