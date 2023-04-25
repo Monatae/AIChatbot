068fad79939aba8305bb8572082724b7ae22a4d1
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-04-26 01:21:05 +0200

    going google

--- bot.py
+++ bot.py
@@ -1,68 +1,42 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
-import socket
-#from transmit import sendaudio, receive_transcript
-from datalab import *
-from twilio.rest import Client
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-
-#twilio constants
-account_sid = 'AC81c0755065e9eba6542ca94a9571ca8e'
-auth_token = '3092f6576db06deb1cac112ea17d86e1'
-client = Client(account_sid, auth_token)
-
-#setting up connection to the speech recognition server
-server_ip = '127.0.0.1'
-server_port = 5001
-server_address = (server_ip, server_port)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((server_address))
-####this is where sendaudio and receive_transcript functions come into play#####
-
-
-#setting up the chatbot
-
-
-
+from google.oauth2 import service_account
+from google.cloud import dialogflow
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+# Set up Dialogflow API credentials
+credentials = service_account.Credentials.from_service_account_file(
+    "friday-381906-3293c69c7a04.json"
+)
+project_id = "friday-381906"
+
+# Create a Dialogflow client
+client = dialogflow.SessionsClient(credentials=credentials)
+
+
+# Define Flask endpoint for Twilio webhook
+@app.route("/webhook", methods=["POST"])
 def webhook():
-    friday = ChatBot("Friday",
-                 read_only = True,
-                 storage_adapter ="chatterbot.storage.SQLStorageAdapter",
-                 database_uri="sqlite:///database.sqlite3"
-                 )
+    # Extract incoming message details from Twilio request
+    incoming_message = request.form["Body"]
+    incoming_number = request.form["From"]
+    session_id = incoming_number
+    response = MessagingResponse()
 
-    #training the chatbot
-    friday.set_trainer(ChatterBotCorpusTrainer)
-    friday.train("chatterbot.corpus.english.quries", "chatterbot.corpus.english.greetings", "chatterbot.corpus.english.conversations")
-    exit_tuple = ('exit', 'see you later', 'bye')
-    incoming_msg = request.values.get('Body', '')
-    resp = MessagingResponse()
-    # Use Chatterbot to generate a response
-    if check_existence(incoming_msg) == False:
-        resp.message('Sorry, I do not fully understand')
-    else:
-        while check_existence(incoming_msg) == True:
-            if incoming_msg.lower() in exit_tuple:
-                resp.message('Bye')
-                break
-            else:
-                response = friday.get_response(incoming_msg)
-                statement = response.text
-                resp.message(statement)
-                break
-            
-    return str(resp)
+    # Send message to Dialogflow
+    session = client.session_path(project_id, session_id)
+    text_input = dialogflow.types.TextInput(
+        text=incoming_message, language_code="en-US"
+    )
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    response = client.detect_intent(session=session, query_input=query_input)
+
+    # Extract response from Dialogflow and send it back to user via Twilio
+    response_text = response.query_result.fulfillment_text
+    response.message(response_text)
+    return str(response)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
-
-
-        
-        
